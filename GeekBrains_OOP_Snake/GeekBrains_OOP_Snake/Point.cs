63de2634e4b366241cb555a893ce25603c3bbdc7
Alexander <alexander_freeman85@mail.ru>
2021-03-27 19:34:21 +0300

    Создание классов

---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains_OOP_Snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
