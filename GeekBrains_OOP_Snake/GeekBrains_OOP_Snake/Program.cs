d79432b10a08a41a961b4e716487686375912f9f
Alexander <alexander_freeman85@mail.ru>
2021-03-28 23:05:15 +0300

    Рисуем линии

---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekBrains_OOP_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point p1 = new Point(1, 3, '*');
+            //p1.Draw();
+
+            //Point p2 = new Point(4, 5, '#');
+            //p2.Draw();
+
+            HorizontalLines hline = new HorizontalLines(0, 20, 0, '+');
+            hline.Draw();
+
+            VerticalLine vline = new VerticalLine(0, 20, 0, '+');
+            vline.Draw();
+
+            VerticalLine vline2 = new VerticalLine(0, 20, 20, '+');
+            vline2.Draw();
+
+            HorizontalLines hline2 = new HorizontalLines(0, 20, 20, '+');
+            hline2.Draw();
+
+            Console.ReadKey();
+        }
+	}
+}
