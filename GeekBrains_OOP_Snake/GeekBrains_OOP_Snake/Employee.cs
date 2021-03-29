1b3172cf6a9d854d8d09f737561ca47073f55f15
Alexander <alexander_freeman85@mail.ru>
2021-03-29 19:05:57 +0300

    Наследование

---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains_OOP_Snake
+{
+    class Employee : Human
+    {
+        public int Salary { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekBrains_OOP_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(83, 25);
+            Console.SetBufferSize(83, 25);
+
+            //Отрисовка рамки
+            HorizontalLines upLine = new HorizontalLines(0, 78, 0, '+');
+            HorizontalLines downLine = new HorizontalLines(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
+            Employee first = new Employee();
+
+            first.Age = 29;
+            first.Name = "Alexander";
+            first.Position = "Head of office";
+            first.Sex = "Man";
+
+
+
+
+            Console.ReadKey();
+        }
+	}
+}
