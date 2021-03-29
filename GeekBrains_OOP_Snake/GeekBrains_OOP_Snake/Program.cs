c66ee41f15450c0a95d6eb1e8b875542e7864087
Alexander <alexander_freeman85@mail.ru>
2021-03-29 20:34:53 +0300

    Абстрагирование

---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeekBrains_OOP_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(83, 25);
+            Console.SetBufferSize(83, 25);
+
+            //Отрисовка рамки
+            HorizontalLines upLine = new HorizontalLines(0, 78, 0, '+');
+            HorizontalLines downLine = new HorizontalLines(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+            Console.ReadKey();
+        }
+	}
+}
