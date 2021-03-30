7cefa025d0be12f90f8c5f2954522ac5fec10e8f
Alexander <alexander_freeman85@mail.ru>
2021-03-30 15:22:29 +0300

    Змейка в движении (принцип инкапсуляции)

---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace GeekBrains_OOP_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(83, 25);
+            Console.SetBufferSize(83, 25);
+            Console.CursorVisible = false;
+
+            //Отрисовка рамки
+            HorizontalLines upLine = new HorizontalLines(0, 78, 0, '+');
+            HorizontalLines downLine = new HorizontalLines(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.ControlKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+            Console.ReadKey();
+        }
+	}
+}
---
+++ GeekBrains_OOP_Snake/GeekBrains_OOP_Snake/Snake.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrains_OOP_Snake
+{
+    class Snake : Figure
+    {
+        Direction Direction;
+        public Snake(Point tail, int length, Direction direction)
+        {
+            Direction = direction;
+            pList = new List<Point>();
+
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, Direction);
+                pList.Add(p);
+            }
+        }
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        private Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, Direction);
+            return nextPoint;
+        }
+
+        public void ControlKey(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    Direction = Direction.LEFT;
+                    break;
+                case ConsoleKey.RightArrow:
+                    Direction = Direction.RIGHT;
+                    break;
+                case ConsoleKey.UpArrow:
+                    Direction = Direction.UP;
+                    break;
+                case ConsoleKey.DownArrow:
+                    Direction = Direction.DOWN;
+                    break;
+            }
+        }
+    }
+}
